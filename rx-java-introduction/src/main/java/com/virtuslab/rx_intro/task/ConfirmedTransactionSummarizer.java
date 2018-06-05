055e86919cd78587b9840f43f04dbc723ccafe2b
konrad <mr.konrad.jakubiec@gmail.com>
2018-06-05 02:22:27 +0200

    added examples + task

---
+++ rx-java-introduction/src/main/java/com/virtuslab/rx_intro/task/ConfirmedTransactionSummarizer.java
@@ -0,0 +1,42 @@
+package com.virtuslab.rx_intro.task;
+
+
+import io.reactivex.Observable;
+import io.reactivex.Single;
+
+import java.math.BigDecimal;
+import java.util.function.Supplier;
+
+/**
+ * ConfirmedTransactionSummarizer is responsible for calculation of total confirmed transactions value.
+ * HINT:
+ * - Use zip operator to match transactions with confirmations. They will appear in order
+ * - Filter only confirmed
+ * - Aggregate value of confirmed transactions
+ *
+ * HINT2:
+ * - add error handling which will wrap an error into SummarizationException
+ *
+ */
+class ConfirmedTransactionSummarizer {
+
+    private final Supplier<Observable<Transaction>> transactions;
+    private final Supplier<Observable<Confirmation>> confirmations;
+
+    ConfirmedTransactionSummarizer(Supplier<Observable<Transaction>> transactions,
+                                   Supplier<Observable<Confirmation>> confirmations) {
+        this.transactions = transactions;
+        this.confirmations = confirmations;
+    }
+
+    Single<BigDecimal> summarizeConfirmedTransactions() {
+        return Single.error(new RuntimeException("Not Implemented Yet"));
+    }
+
+    static class SummarizationException extends RuntimeException {
+
+        public SummarizationException(String message) {
+            super(message);
+        }
+    }
+}

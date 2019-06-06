e93350df3e051842a8f4b24ac77c8b1ebd720c86
konrad <mr.konrad.jakubiec@gmail.com>
2019-06-06 18:59:35 +0200

    fixed backpressure example

--- rx-java-introduction/src/main/java/com/virtuslab/rx_intro/examples/Backpressure.java
+++ rx-java-introduction/src/main/java/com/virtuslab/rx_intro/examples/Backpressure.java
@@ -7,45 +7,43 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-class Backpressure {
+class ObservableBackpresure {
 
-    private final static Logger LOG = LoggerFactory.getLogger(Backpressure.class);
+    private final static Logger LOG = LoggerFactory.getLogger(ObservableBackpresure.class);
 
-    private static class ObservableBackpresure {
+    public static void main(String[] args) throws InterruptedException {
+        Observable.range(1, 512)
+                .map(id -> "ObservableString_" + id)
+                .doOnNext(string -> LOG.info("Got {}", string))
+                .observeOn(Schedulers.io())
+                .subscribe(
+                        string -> {
+                            LOG.info("Consumed {}", string);
+                            Thread.sleep(50);
+                        }
+                );
 
-        public static void main(String[] args) throws InterruptedException {
-            Observable.range(1, 512)
-                    .map(id -> "ObservableString_" + id)
-                    .doOnNext(string -> LOG.info("Got {}", string))
-                    .observeOn(Schedulers.io())
-                    .subscribe(
-                            string -> {
-                                LOG.info("Consumed {}", string);
-                                Thread.sleep(50);
-                            }
-                    );
-
-            Thread.sleep(10000);
-        }
-
+        Thread.sleep(10000);
     }
 
-    private static class FlowableBackpressure {
+}
 
-        public static void main(String[] args) throws InterruptedException {
-            Flowable.range(1, 512)
-                    .map(id -> "FlowableString_" + id)
-                    .doOnNext(string -> LOG.info("Got {}", string))
-                    .observeOn(Schedulers.io())
-                    .subscribe(
-                            string -> {
-                                LOG.info("Consumed {}", string);
-                                Thread.sleep(50);
-                            }
-                    );
+class FlowableBackpressure {
+    private final static Logger LOG = LoggerFactory.getLogger(FlowableBackpressure.class);
 
-            Thread.sleep(10000);
-        }
+    public static void main(String[] args) throws InterruptedException {
+        Flowable.range(1, 512)
+                .map(id -> "FlowableString_" + id)
+                .doOnNext(string -> LOG.info("Got {}", string))
+                .observeOn(Schedulers.io())
+                .subscribe(
+                        string -> {
+                            LOG.info("Consumed {}", string);
+                            Thread.sleep(50);
+                        }
+                );
 
+        Thread.sleep(10000);
     }
+
 }
